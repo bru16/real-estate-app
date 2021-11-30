efb4cd08edff8fb94155ad079ee353ec6867fbb2
Bruno <bru_16@hotmail.com.ar>
2021-11-30 13:24:31 -0300

    feat: search property by filters

--- components/SearchFilters.js
+++ components/SearchFilters.js
@@ -1,12 +1,44 @@
-import { Flex } from '@chakra-ui/layout'
+import { Box, Flex } from '@chakra-ui/layout'
+import { Select } from '@chakra-ui/select'
+import router from 'next/router'
 import React, { useState } from 'react'
+import { filterData, getFilterValues } from '../utils/filterData'
+
 
 const SearchFilters = () => {
-    const [filters, setFilters] = useState({})
+    const [filters, setFilters] = useState(filterData)
+
+    const searchProperties = (filterValues) => {
+        const path = router.pathname;
+        const { query } = router;
+
+        const values = getFilterValues(filterValues);
+
+        values.forEach((item) => {
+            query[item.name] = item.value;
+        })
+
+        router.push({ pathname: path, query })
+
+    }
+
 
     return (
         <Flex bg="gray.100" p="4" justifyContent="center" flexWrap="wrap">
-
+            {filters.map((filter) =>
+                <Box key={filter.queryName}>
+                    <Select
+                        placeholder={filter.placeholder}
+                        w="fit-content"
+                        p="2"
+                        onChange={(e) => searchProperties({ [filter.queryName]: e.target.value })}
+                    >
+                        {filter?.items?.map((item) => (
+                            <option value={item.value} key={item.value}>{item.name}</option>
+                        ))}
+                    </Select>
+                </Box>
+            )}
         </Flex>
     )
 }
